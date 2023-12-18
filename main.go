08f30f0a38250c656bfb237375e57449e014a561
Yura Gushchin <yura.gushchin@gmail.com>
2023-12-18 19:48:01 +0400

    update

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"time"
+	"encoding/json"
+	"flag"
 
-	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/eclipse/paho.mqtt.golang"
 )
 
 type Config struct {
@@ -15,44 +18,65 @@ type Config struct {
 	Topic    string
 	Username string
 	Password string
+	Debug bool
 }
 
+
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	// topic := string(msg.Topic())
+	cmd := string(msg.Payload())
+	
+	if cmd == "SLEEP"{
+		log.Println("GO WINDOWS SLEEP")
+		cmd := exec.Command("rundll32.exe", "powrprof.dll,SetSuspendState", "0,1,0")
+		err := cmd.Run()
+		if err != nil {
+			log.Println(err)
+		}	
+	}
+}
+
+
 func main() {
-	// Чтение конфигурации из файла
+	// Read config from file
 	config := loadConfig("config.json")
 
-	// Подключение к MQTT брокеру
-	client := MQTT.NewClient(MQTT.NewClientOptions().
-		SetClientID(fmt.Sprintf("client-%s", config.Topic)).
-		AddBroker(config.Broker).
-		SetUsername(config.Username).
-		SetPassword(config.Password))
-	err := client.Connect()
-	if err != nil {
-		fmt.Println(err)
+	if (config.Debug){
+		mqtt.DEBUG = log.New(os.Stdout, "", 0)
+		mqtt.ERROR = log.New(os.Stdout, "", 0)
+	}
+
+	opts := mqtt.NewClientOptions().AddBroker(config.Broker).SetClientID("gotrivial")
+	opts.SetUsername(config.Username)
+	opts.SetPassword(config.Password)
+	opts.SetKeepAlive(2 * time.Second)
+	opts.SetDefaultPublishHandler(f)
+	opts.SetPingTimeout(1 * time.Second)
+
+	num := flag.Int("num", 1, "The number of messages to publish or subscribe (default 1)")
+
+	c := mqtt.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	if token := c.Subscribe(config.Topic, 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
 		os.Exit(1)
 	}
 
-	// Подписываемся на топик
-	err = client.Subscribe(config.Topic, 0, func(c MQTT.Client, msg MQTT.Message) {
-		// Если сообщение содержит значение "sleep", то отправляем компьютер в сон
-		if string(msg.Payload()) == "sleep" {
-			fmt.Println("Отправляем компьютер в сон")
-			cmd := exec.Command("rundll32.exe", "powrprof.dll,SetSuspendState", "0,1,0")
-			err := cmd.Run()
-			if err != nil {
-				fmt.Println(err)
-			}
-		}
-	})
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	receiveCount := 0
+	choke := make(chan [2]string)
+	
+	for receiveCount < *num {
+		incoming := <-choke
+		log.Printf("RECEIVED TOPIC: %s MESSAGE: %s\n", incoming[0], incoming[1])
+		receiveCount++
 	}
 }
 
 func loadConfig(filename string) *Config {
-	// Открытие файла
+	// Open file 
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println(err)
@@ -60,7 +84,7 @@ func loadConfig(filename string) *Config {
 	}
 	defer file.Close()
 
-	// Чтение конфигурации из файла
+	// Read config from file
 	decoder := json.NewDecoder(file)
 	var config Config
 	err = decoder.Decode(&config)
